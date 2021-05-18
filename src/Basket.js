dc94887e7dbecdc1a71ed648954e73b1019de3e9
Riddhi <riddhipatel.csu@gmail.com>
2021-05-17 18:31:46 -0700

    Second commit

--- src/Basket.js
+++ src/Basket.js
@@ -10,8 +10,8 @@ export default function Basket(props) {
                     return (
                         <div className="border d-flex align-items-start">
                             <img src={prod.image} alt={prod.desc} width='150' />
-                            <p className="qty d-inline mx-5 justify-content-center">Quantity: {prod.value}</p>
-                            <p className="px-4">{prod.desc}</p>
+                            <p className="qty d-inline mx-5 mt-5 pt-3 justify-content-center">Quantity: {prod.value}</p>
+                            <p className="px-4 pt-3 mt-5">{prod.desc}</p>
                         </div>
                     )
                 }
--- src/navbar.js
+++ src/navbar.js
@@ -3,16 +3,18 @@ import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import DisplayProducts from "./displayProducts";
 import Basket from "./Basket";
 import { Link, BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import './App.css';
 
 function Nav(props) {
+    const myStyle = {
+        color: "#61dafb"
+    }
     return (
         <Router>
             <div>
                 <header className="App-header ps-5 pe-5">
                     <Link to="/">
-                        <h2 className="text-white">Shop 2 <span className="ps-2 pe-2 pb-1 bg-light text-dark rounded-circle" id="r-color">R</span>eact</h2>
+                        <h2 className="text-white rColor">Shop 2 <span className="ps-2 pe-2 pb-1 bg-light text-dark rounded-circle" id="" style={{color: "#61dafb"}}>R</span>eact</h2>
                     </Link>
                     <Link to="/shopCart">
                         <div className="text-white">
